846765683abf3b317ed9d8f314404686f8d21c99
caamaann <36650325+caamaann@users.noreply.github.com>
2020-07-11 17:14:23 +0700

    menambahkan client master

---
+++ master-service/src/test/java/distribusiTiket/masterservice/MasterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package distribusiTiket.masterservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MasterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
