57eca0daa716238933afa1e3311899d1c3347ac7
Ilham Gibran <ilgibrz@gmail.com>
2020-07-16 23:51:23 +0700

    Add functionality to request ticket in need

--- retail-service/src/main/java/xyz/ilhamgibran/spring/retailservice/Controller/OrderController.java
+++ retail-service/src/main/java/xyz/ilhamgibran/spring/retailservice/Controller/OrderController.java
@@ -1,13 +1,21 @@
 package xyz.ilhamgibran.spring.retailservice.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 import xyz.ilhamgibran.spring.retailservice.Model.*;
 import xyz.ilhamgibran.spring.retailservice.Repository.*;
 
 import javax.servlet.http.HttpServletResponse;
+import java.net.InetAddress;
+import java.net.URI;
+import java.net.UnknownHostException;
 import java.sql.Date;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -35,7 +43,17 @@ public class OrderController {
     @Autowired
     private TicketSeatRepository ticketSeatRepository;
 
+    @Autowired
+    DiscoveryClient discoveryClient;
+
+    @Autowired
+    Environment environment;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
     InputFormOrder form = new InputFormOrder();
+    URI micro2URI;
 
     @GetMapping(path = "/")
     public String showOrderForm(Model model){
@@ -83,13 +101,33 @@ public class OrderController {
             model.addAttribute("status", "Data Not Found");
             return "index";
         }else{
-//            System.out.println("Origin City is : " + ticket.getOrigin().getCityName());
-//            System.out.println("Destination City is : " + ticket.getDestination().getCityName());
-//            System.out.println("Flight Class City is : " + ticket.getSeatClass().getClassName());
-//            System.out.println("Departure Date : " + input.getDepartureDateSQLFormat());
-//            System.out.println("Ticket available : " + ticket.getStock() + " Ticket");
             if(ticket.getStock() < input.getAdultPass()){
-                // Request ke Tetangga
+                String port = environment.getProperty("server.port");
+                String comp = "";
+                List<ServiceInstance> list =
+                        discoveryClient.getInstances("retail-service");
+
+                try {
+                    for(ServiceInstance host:list){
+                        String params = "?origin="+ticket.getOrigin().getCityId()
+                                +"&des="+ticket.getDestination().getCityId()
+                                +"&date="+ticket.getFlightDate()
+                                +"&class="+ticket.getSeatClass().getClassId()
+                                +"&passager="+(input.getAdultPass()-ticket.getStock());
+
+                        ServiceInstance service2 = host;
+                        this.micro2URI = service2.getUri();
+                        comp = micro2URI.getHost()+":"+micro2URI.getPort();
+                        if(!comp.equals(InetAddress.getLocalHost().getHostName()+":"+port)){
+                            ResponseEntity<Ticket> movieSummary = restTemplate.getForEntity(micro2URI +
+                                    "/api/ticket/get"+params, Ticket.class);
+                        }
+                    }
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                }
+            }else{
+                this.micro2URI = null;
             }
             List<String> listName = new ArrayList<>();
 
@@ -98,7 +136,6 @@ public class OrderController {
 
             model.addAttribute("input", input);
             model.addAttribute("ticket", ticket);
-
             return "saved";
         }
     }
@@ -110,35 +147,36 @@ public class OrderController {
 //        System.out.println(input.getOrigin() == 0 ? "NULL" : input.getOrigin());
 //        System.out.println(input.getDestination() == 0 ? "NULL" : input.getDestination());
 //        System.out.println(input.getDepartureDate() == null ? "NULL" : input.getDepartureDate());
+        System.out.println("This MicroService2 : " + this.micro2URI);
 
-        Ticket ticket = ticketRepository.searchAvailability(input.getDepartureDateSQLFormat()
-                ,input.getOrigin(),input.getDestination(), input.getFlightClass());
-
-        if(ticket == null){
-            httpServletResponse.setHeader("Location", "/");
-            httpServletResponse.setStatus(302);
-        }else{
-            double amount = input.getAdultPass()*ticket.getPrice();
-
-            // Save Order
-            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
-            java.sql.Date now = new Date(Calendar.getInstance().getTime().getTime());
-            Orders myOrder = new Orders(now, amount);
-            ordersRepository.save(myOrder);
-
-            // Save Order Ticket
-            List<TicketSeat> seat = ticketSeatRepository.getSeatByStatus(ticket.getTicketId(), 1);
-            int idx = 0;
-            for(String name : input.getPassagerName()){
-                orderTicketRepository.save(new OrderTicket(myOrder, seat.get(idx), name));
-                seat.get(idx).setAvailability(0);
-                ticketSeatRepository.save(seat.get(idx));
-                idx += 1;
-            }
-
-            ticket.setStock(ticket.getStock()-input.getAdultPass());
-            ticketRepository.save(ticket);
-        }
+//        Ticket ticket = ticketRepository.searchAvailability(input.getDepartureDateSQLFormat()
+//                ,input.getOrigin(),input.getDestination(), input.getFlightClass());
+//
+//        if(ticket == null){
+//            httpServletResponse.setHeader("Location", "/");
+//            httpServletResponse.setStatus(302);
+//        }else{
+//            double amount = input.getAdultPass()*ticket.getPrice();
+//
+//            // Save Order
+//            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//            java.sql.Date now = new Date(Calendar.getInstance().getTime().getTime());
+//            Orders myOrder = new Orders(now, amount);
+//            ordersRepository.save(myOrder);
+//
+//            // Save Order Ticket
+//            List<TicketSeat> seat = ticketSeatRepository.getSeatByStatus(ticket.getTicketId(), 1);
+//            int idx = 0;
+//            for(String name : input.getPassagerName()){
+//                orderTicketRepository.save(new OrderTicket(myOrder, seat.get(idx), name));
+//                seat.get(idx).setAvailability(0);
+//                ticketSeatRepository.save(seat.get(idx));
+//                idx += 1;
+//            }
+//
+//            ticket.setStock(ticket.getStock()-input.getAdultPass());
+//            ticketRepository.save(ticket);
+//        }
 
         return "postOrder";
     }
--- retail-service/src/main/java/xyz/ilhamgibran/spring/retailservice/RetailServiceApplication.java
+++ retail-service/src/main/java/xyz/ilhamgibran/spring/retailservice/RetailServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.concurrent.Executor;
 
@@ -25,6 +26,11 @@ public class RetailServiceApplication {
 		executor.setThreadNamePrefix("orderMy-");
 		executor.initialize();
 		return executor;
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
